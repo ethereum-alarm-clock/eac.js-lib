89b080d9de7a99360ef5880574447df0c6266cc1
Anthony <adibas03@ymail.com>
2018-05-14 14:22:54 +0100

    update deploy.sh, addpackage.json to lib

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,7 @@ cd ..
 echo "Moving the generated contract files..."
 rm -Rfv lib/build/*
 cp -Rfv ethereum-alarm-clock/build/* lib/build/
+cp -fv ethereum-alarm-clock/package.json lib/build/ethereum-alarm-clock.json
 
 node ./extractContractsInfo.js development
 mv -fv contracts.json lib/assets/development.json || true
