7d152ccaf6bd6c44e8caf6db0e7dd565d4a4484d
lsaether <Lsaether@protonmail.com>
2018-07-30 13:42:23 +0300

    attempt to fix tests 5/?

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ cd ..
 
 echo "Moving the generated contract files..."
 rm -Rfv static/build/*
-cp -Rfv ethereum-alarm-clock/build/* static/build/
+cp -Rfv ethereum-alarm-clock/build/* static/build/contracts
 cp -fv ethereum-alarm-clock/package.json static/build/ethereum-alarm-clock.json
 ls static/build
 
