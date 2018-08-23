bc8d0e86d2b25f754d37bab945321d5992b97988
lsaether <Lsaether@protonmail.com>
2018-08-23 19:36:08 +0100

    minor upkeep

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const TxRequest = require("./lib/txRequest")
 const Util = require("./lib/util")
 const RequestData = require("./lib/txRequest/requestData")
 const version = require('./package.json').version;
-const contracts = require("./lib/build/ethereum-alarm-clock.json").version;
+const contracts = require("./package.json").dependencies["@ethereum-alarm-clock/contracts"];
 
 /**
  * 
